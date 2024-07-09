342ee2347a2ba09db5c30e7225889be29ddd9157
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2024-07-09 05:57:25 +0530

    Add files via upload

---
+++ git (1).py
@@ -0,0 +1,56 @@
+# Initializing a new Git repository
+'''
+    git init
+'''
+# Check the status of your repository
+'''
+    git status
+'''
+# Add files to the staging area
+'''
+    git add .
+'''
+# Commit changes to the repository
+''' 
+    git commit -m "Commit message"
+'''
+# Check the history of commits
+''' 
+    git log
+'''
+# Add a remote repository (if not already added)
+''' 
+    git remote add origin <repository_url>
+'''
+# Push changes to the remote repository
+''' 
+    git push origin main
+'''
+# Pull changes from the remote repository to update your local repository
+''' 
+    git pull origin <branch_name>
+'''
+# Create a new branch
+''' 
+    git branch <branch_name>
+'''
+# Switch to a different branch
+''' 
+    git checkout <branch_name>
+'''
+# Create and switch to a new branch in one command
+''' 
+    git checkout -b <branch_name>
+'''
+# Merge another branch into the current branch
+''' 
+    git merge <branch_name>
+''' 
+# Delete a branch
+'''
+    git branch -d <branch_name>
+'''
+# View all branches
+''' 
+    git branch
+''' 
